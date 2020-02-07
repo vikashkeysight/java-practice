0215bbd467fe1be042142bf661f0f202a0edacb1
praveen <praveen@DESKTOP-DQ9JBUC>
2020-02-07 14:21:46 +0530

    3rd change

--- src/com/test/demo/TestDemo1.java
+++ src/com/test/demo/TestDemo1.java
@@ -5,6 +5,8 @@ public class TestDemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test Demo");
+		System.out.println("Hello Eclipse");
+		System.out.println("Hello Java");
 	}
 
 }
